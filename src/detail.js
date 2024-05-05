a9ae9b3dc0010c4eabbc5efe597292c46da6e9c8
pledge24 <104922729+pledge24@users.noreply.github.com>
2024-05-05 18:39:28 +0900

    ✨ update  수정, 삭제 버튼 추가 및 기타 등등...
    수정, 삭제 버튼 추가
    메인페이지 -> 상세페이지 이동 연결
    댓글 비밀번호 SHA-256으로 암호화
    비밀번호 일치 여부 확인 기능 추가

--- src/detail.js
+++ src/detail.js
@@ -1,10 +1,10 @@
 const $detailContainer = document.querySelector('#detailed_introduction');
 const $reviewBox = document.querySelector('#reviewbox');
+const $encryptScript = document.createElement('script');
+const movie_id = new URLSearchParams(location.search).get("id");
 
 let movie;
 let movieConfig;
-
-let movie_id;
 
 // 임시로 본인 API키 사용 중....
 const options = {
@@ -15,82 +15,162 @@ const options = {
     }
 };
 
-init();
+// 페이지 진입 시 초기화.
+init();    
 
 function init() {
 
-    const URLSearch = new URLSearchParams(location.search);
-    //movie_id = URLSearch.get("id");
-    movie_id = 278;
+    $encryptScript.src = "https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.min.js";
+    document.head.appendChild($encryptScript);
+
     fetch('https://api.themoviedb.org/3/configuration', options)
         .then(response => response.json())
         .then(response => {
             movieConfig = response.images;
             
-            // 영화 정보 가져옴
             fetch(`https://api.themoviedb.org/3/movie/${movie_id}?language=ko-KR`, options)
                 .then(response => response.json())
                 .then(response => {
                     movie = response;
-                    console.log(movie);
-                    fetchMovieDetails();
+                    //console.log(movie);
+                    loadMovieDetails();     // 영화의 자세한 정보를 가져온다.
+                    loadReviews();          // 해당 영화의 리뷰를 가져온다.
                 })
                 .catch(err => console.error(err));
         })
         .catch(err => console.error(err));
-
-    // 영화 id를 통해 review를 localstorage에서 가져온다.
-    if(localStorage.getItem(movie_id) != ''){
-        let reviewList = JSON.parse(localStorage.getItem(movie_id));
-        
-        //console.log(reviewList);
-
-        // 가져온 커맨드를 커맨드 창에 추가한다.
-        loadReviews(reviewList);
-    }
-
 }
 
-// 영화 상세페이지 정보 입력.
-function fetchMovieDetails() {
+// 영화 상세페이지 정보 입력 함수
+function loadMovieDetails() {
     
     let _fullUrl = movieConfig.base_url + movieConfig.poster_sizes[4] + movie.poster_path;
 
     $detailContainer.querySelector('img').src = _fullUrl;
     $detailContainer.querySelector('#movie_title').textContent = movie.original_title;
     $detailContainer.querySelector('#movie_contents').textContent = movie.overview; 
+     
 }
 
-// 적힌 리뷰들 불러오는 함수.
-function loadReviews(reviewList){
-    // 아무것도 들어있지 않으면 리턴.
-    if(reviewList === null) return;
+// 저장된 리뷰들을 불러오는 함수
+function loadReviews(){
 
+    if(localStorage.getItem(movie_id) === null) return;
+    
+    let reviewList = JSON.parse(localStorage.getItem(movie_id));
     for(let i = 0; i < reviewList.length; i++){
         //console.log(`${reviewList[i].password}, ${reviewList[i].comment}`);
-        appendReview(reviewList[i]);
+        appendReview(reviewList[i], i);
     }
 }
 
 // 1개의 리뷰를 리뷰칸에 추가한다.
-function appendReview(review_info) {
+function appendReview(review_info, review_id) {
     const $reviewCotainer = document.createElement('div');
     $reviewCotainer.setAttribute("class", "review");
-
+    $reviewCotainer.setAttribute("value", review_info.password);
+    $reviewCotainer.setAttribute("id", review_id);
+    //console.log(review_info);
     let temp_html = `
-        <div class="user_info" value="${review_info.pw}">
-            <div>user_name: Dal</div>
-            <div>score: ⭐⭐⭐⭐⭐</div>
+        <div class="user_info">
+            <div>${review_info.nickname}</div>
+            <div>score: ${review_info.rate}</div>
         </div>
+        <img src="assets/edit-button.png" class="edit-btn" style="width: 50px; height: 50px;"></img>
+        <img src="assets/delete-button.png" class="delete-btn" style="width: 50px; height: 50px;"></img>
         <div class="review_comments">
-            ${review_info.comment}
+        <input type="textarea" value="${review_info.comment}" disabled>  
         </div>
         <br>
     `;
 
     $reviewCotainer.innerHTML = temp_html;
 
-    //addButtonEvent($reviewCotainer);
+    addButtonEvent($reviewCotainer);
      
     $reviewBox.append($reviewCotainer);
+}
+
+function isCorrectPassword(password_correct){
+    const password_try = prompt('비밀번호를 입력하세요:');
+
+    if (password_try && sha256(password_try) === password_correct) {
+        return true;
+    }
+    
+    alert("비밀번호가 일치하지 않습니다.");
+
+    return false;
+}
+
+function addButtonEvent($commentCotainer){
+    const $editButton = $commentCotainer.querySelector('.edit-btn');
+    const $deleteButton = $commentCotainer.querySelector('.delete-btn');
+    const $commentInput = $commentCotainer.querySelector('input');
+
+    $editButton.addEventListener("click", function(e){
+        let isDisabled = $commentInput.disabled;
+        if(confirm("s")){
+            alert("true");
+        }
+        else{
+            alert("false");
+        }
+        alert("edit button clicked!");
+        //console.log($commentInput.getAttribute('value'));
+        // 수정 요청 시 if문, 수정 완료 시 else문 진입.
+        if(isDisabled){
+
+            if(isCorrectPassword($commentCotainer.getAttribute('value'))){
+                $commentInput.disabled = false;
+            }
+            
+        }
+        else{
+            $commentInput.disabled = true;
+
+            let arr = JSON.parse(window.localStorage.getItem(movie_id));
+            let idx = $editButton.parentElement.getAttribute('id');
+
+            let text_um = $commentInput.value;
+            console.log(text_um);
+            arr[idx].comment = text_um;
+            window.localStorage.setItem(movie_id, JSON.stringify(arr));
+
+            alert("edit completed!");
+        }
+        
+    })
+    
+    $deleteButton.addEventListener("click", function(e){
+        //e.preventDefault();
+        
+        if(!isCorrectPassword($deleteButton.parentElement.getAttribute("value"))) return;
+
+        let sdssdsdsdd = confirm("정말로 삭제하시겠습니까?");
+        console.log(typeof sdssdsdsdd);
+        if(sdssdsdsdd){
+            alert("if condition: true");
+        }
+        else{
+            alert("if condition: false");
+        }
+        if("true" === confirm("정말로 삭제하시겠습니까?")){          
+            // $deleteButton.parentElement.remove();
+            
+            let arr = JSON.parse(window.localStorage.getItem(movie_id));
+            let idx = $deleteButton.parentElement.getAttribute('id');
+            console.log(idx);
+            arr.splice(idx, 1);
+            window.localStorage.setItem(movie_id, JSON.stringify(arr));
+
+            alert("This comment has been deleted!!");
+            //console.log("test");
+            window.location.reload();
+        }   
+        
+        // let message = "2323";
+        // let hash = sha256(message);
+        // console.log(hash);
+    })
 }
